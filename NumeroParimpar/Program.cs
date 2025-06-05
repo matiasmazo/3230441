d77b79d8d4f52ea43de506224a46808c57e0afba
matiasmazo <morenomazo84@gmail.com>
2025-06-05 15:37:14 -0500

    proyecto par impar

---
+++ NumeroParimpar/Program.cs
@@ -0,0 +1,21 @@
+﻿
+do
+{
+    Console.WriteLine("Ingrese un numero");
+var numero = int.Parse(Console.ReadLine());
+
+if (numero % 2 == 0)
+{
+    Console.WriteLine("El numero es par");
+  }
+else
+{
+ Console.WriteLine("el numero es impar");
+}
+
+ } while (true) ;
+
+
+
+
+    
